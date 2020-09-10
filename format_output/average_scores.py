a57b5bb0ca8327e95827fa3935e4b8bb768313bb
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-09 19:16:21 -0500

    Wrote first test.

---
+++ format_output/average_scores.py
@@ -0,0 +1,11 @@
+"""
+1.  Write tests to read one person's information: Include first & last name, age, and find the average of three numbers 0-100 (store within a variable)
+2. Create a project named Module 3, add directories test and format_output
+3. Within the test directory, add a unit test called 'test_average_scores.py
+4. Within the format_output directory, add average_scores.py
+
+"""
+
+
+def average():
+    pass

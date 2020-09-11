a521b7c47b2322ac8bccdfded22ac451e8937b66
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-10 19:11:51 -0500

    Added comments and docstring. Code functions properly.

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,28 +1,27 @@
 """
-1.  Write tests to read one person's information: Include first & last name, age, and find the average of three numbers 0-100 (store within a variable)
-2. Create a project named Module 3, add directories test and format_output
-3. Within the test directory, add a unit test called 'test_average_scores.py
-4. Within the format_output directory, add average_scores.py
-
+Program: average_scores.py
+Author: Grayson Hardin
+Last date modified: 9/10/2020
 """
 
 
 def average():
+    # Each variable down below (score1 - score3) will collect user input.
     score1 = int(input("Enter first number here:"))
     score2 = int(input("Enter second number here:"))
     score3 = int(input("Enter third number here:"))
-    average_scores = (score1 + score2 + score3) / 3
+    average_scores = (score1 + score2 + score3) / 3  # Here the we perform calculation on the previous variables.
     return average_scores
 
 
 def main():
+    # Like the previous, we collect another set of variables (last name, first name, and age)
     last_name = input("Enter last name:")
     first_name = input("Enter first name:")
     age = input("Enter age:")
     average_score = average()
-    print(f'{last_name}, {first_name} age: {age} average grade: {average_score}')
+    print(f'{last_name}, {first_name} age: {age} average grade: {average_score}') # return the values and format accordingly.
 
 
 if __name__ == '__main__':
     main()
-
